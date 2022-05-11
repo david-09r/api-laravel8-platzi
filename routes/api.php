b82fa6eb195e11fd6d5707a06c7f340391afc048
David Rodriguez <davidchu1236@gmail.com>
2022-05-11 15:36:39 -0500

    Agregando autorizacion

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v2')->group(function () {
     Route::apiResource('posts',
         \App\Http\Controllers\Api\V2\PostController::class
-    )->only(['index', 'show', 'destroy']);
+    )->only(['index', 'show', 'destroy']
+    )->middleware('auth:sanctum');
 });
